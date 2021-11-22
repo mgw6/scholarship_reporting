bd377608c55104b567c13e2ba8af0522ace2b3b9
MacGregor <magwin2012@gmail.com>
2021-11-22 10:19:12 -0500

    Updated comments

--- group_recips.py
+++ group_recips.py
@@ -1,6 +1,11 @@
-#MacGregor Winegard
-#11/5/2021 
-# This merges clean output with the 
+"""
+MacGregor Winegard
+11/5/2021 
+This program goes through the list of Scholarship recipients and groups them by scholarship. 
+Output is such that column one is the scholarship and column two is each recipient, delimited by '.'
+
+I copy and pasted this into the main excel file and then used the split text option to break them into multiple columns
+"""
 
 import pandas as pd
 
@@ -13,8 +18,11 @@ class XL_work:
     
     
 if __name__ == '__main__':
-    recip_list = XL_work.get_xl_sheet("XL_Sheets/Advancement Report 10.22.2021.xlsx")
-    schol_name = "Description"
+    recip_list = XL_work.get_xl_sheet("XL_Sheets/Update 11.17.2021.xlsx")
+    #TODO: Make a Tkinter option for this. 
+    
+    
+    schol_name = "RFRBASE_FUND_TITLE_LONG" #This was changed on the 11.17 update. 
     
     name_list = ['First Name', 'Last Name']
     
@@ -51,7 +59,8 @@ if __name__ == '__main__':
     
     test = pd.DataFrame.from_dict(list_for_df)
     
-    test.to_excel("group_output.xlsx", index = False)
+    test.to_excel("11.17_update_group.xlsx", index = False)
+    print("Output saved")
     
     # Using this output, I this in Excel:
     # https://www.adinstruments.com/support/knowledge-base/how-can-comma-separated-list-be-converted-cells-column-lt
